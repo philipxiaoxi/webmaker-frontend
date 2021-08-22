aea5a67611624511439c11b1ed2e1a54130b99ae
DIY熙 <xswork@qq.com>
2021-08-22 14:43:18 +0800

    feat 修改事件为emit

--- src/components/Note.vue
+++ src/components/Note.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="width: 100%;height:100%;">
-        <mavon-editor style="width: 100%;height:100%;" v-model="$parent.$parent.item.note" ></mavon-editor>
+        <mavon-editor v-model="value" style="width: 100%;height:100%;" @input="$emit('input', $event.target.value)" ></mavon-editor>
     </div>
 </template>
 
@@ -9,6 +9,10 @@ export default {
     name: 'Note',
     components: {},
     props: {
+        value: {
+            type: String,
+            default: ''
+        }
     },
     data() {
         return {
--- src/views/Editor.vue
+++ src/views/Editor.vue
@@ -35,7 +35,7 @@
             direction="rtl"
             size='50%'
             :modal='false'>
-            <note :note="item.note"></note>
+            <note :value="item.note" v-model="item.note"></note>
         </el-drawer>
     </div>
 </template>
