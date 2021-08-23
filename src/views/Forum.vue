b7107de5982ecd293266fa1f97e2f68551679fb1
DIY熙 <xswork@qq.com>
2021-08-23 20:51:05 +0800

    feat 新增论坛页面

--- src/views/Forum.vue
+++ src/views/Forum.vue
@@ -1,14 +1,54 @@
 <template>
     <div class="container">
-        <h1>论坛</h1>
+        <h1>欢迎来到 CodeShare Forum（建设中）</h1>
+        <p>你可以在此交流代码片段或使用CodeShare中遇到的问题、与他人分享使用经验。提问前请先阅读《提问的智慧》</p>
+        <forum-list :data='dataList'></forum-list>
     </div>
 </template>
 
 <script>
+import ForumList from '../components/ForumList/ForumList.vue'
 export default {
+    components: { ForumList },
     data() {
         return {
-            form: {}
+            form: {},
+            dataList: [
+                {
+                    id: 1,
+                    title: '一分钟教会你codeshare的正确用法！',
+                    author: '黄鉴熙',
+                    topic: '知识小课堂',
+                    showPreface: true,
+                    replyNum: 12,
+                    preface: '大家好，我是大熙哥今天教会你们如何使用codeshare，代码分享平台，CodeShare网站，能够保存前端代码片段，能够实实时修改，实时预览，在每个代码片段中提供笔记的功能，介绍代码片段的实现方式或者使用方式，通过这种的方式去学习前端。另外还可以提供单页html静态或者动态前端网页链接供开发者使用。分享你的代码片段，享受开源的互利共赢的乐趣。'
+                },
+                {
+                    id: 2,
+                    title: '咸鱼云的登录窗口自定义输入框的设计，很漂亮，不来看看你血亏！',
+                    author: '莫锦涛',
+                    topic: '知识小课堂',
+                    showPreface: true,
+                    replyNum: 6,
+                    preface: '大家好，我是大熙哥今天教会你们如何使用codeshare，代码分享平台，CodeShare网站，能够保存前端代码片段，能够实实时修改，实时预览，在每个代码片段中提供笔记的功能，介绍代码片段的实现方式或者使用方式，通过这种的方式去学习前端。另外还可以提供单页html静态或者动态前端网页链接供开发者使用。分享你的代码片段，享受开源的互利共赢的乐趣。'
+                },
+                {
+                    id: 3,
+                    title: '测试2333333333333333',
+                    author: '莫锦涛',
+                    topic: '知识小课堂',
+                    replyNum: 122,
+                    preface: '大家好，我是大熙哥今天教会你们如何使用codeshare，代码分享平台，CodeShare网站，能够保存前端代码片段，能够实实时修改，实时预览，在每个代码片段中提供笔记的功能，介绍代码片段的实现方式或者使用方式，通过这种的方式去学习前端。另外还可以提供单页html静态或者动态前端网页链接供开发者使用。分享你的代码片段，享受开源的互利共赢的乐趣。'
+                },
+                {
+                    id: 4,
+                    title: '测试23333333333333335666666666666',
+                    author: '莫锦涛',
+                    topic: '知识小课堂',
+                    replyNum: 999,
+                    preface: '大家好，我是大熙哥今天教会你们如何使用codeshare，代码分享平台，CodeShare网站，能够保存前端代码片段，能够实实时修改，实时预览，在每个代码片段中提供笔记的功能，介绍代码片段的实现方式或者使用方式，通过这种的方式去学习前端。另外还可以提供单页html静态或者动态前端网页链接供开发者使用。分享你的代码片段，享受开源的互利共赢的乐趣。'
+                }
+            ]
         }
     },
     mounted() {
