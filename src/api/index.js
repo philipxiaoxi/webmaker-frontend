ec26c84c0b8c69836623844015deffcd2ab36e01
DIY熙 <xswork@qq.com>
2021-08-24 16:09:54 +0800

    feat 获取论坛接口引入总接口文件

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,13 @@
 const snippet = require('./snippet')
 const user = require('./user')
 const snippetProject = require('./snippetProject')
+const forum = require('./forum')
 const API = {
     server: 'http://www.xiaotao2333.top:8000/',
     snippet: snippet,
     user: user,
     snippetProject: snippetProject,
+    forum: forum,
     getServer() {
         return this.server || location.origin
     }
