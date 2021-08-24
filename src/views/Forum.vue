9dc07a777d1c11cff536e96ae7275984f9ca20e1
DIY熙 <xswork@qq.com>
2021-08-24 22:13:23 +0800

    chore 修改标题

--- src/views/Forum.vue
+++ src/views/Forum.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
-        <h1>欢迎来到 CodeShare Forum(测试页面)</h1>
-        <p>你可以在此交流代码片段或使用CodeShare中遇到的问题、与他人分享使用经验。提问前请先阅读《提问的智慧》</p>
+        <h1>欢迎来到 CodeShare Forum</h1>
+        <p>你可以在此交流代码片段或使用CodeShare中遇到的问题、与他人分享使用经验。提问前请先阅读<el-link type="primary" @click="$router.push({ path: '/forumpage', query: { id: 4 } })">《提问的智慧》</el-link></p>
         <forum-list @click="goTo" :data='dataList'></forum-list>
     </div>
 </template>
