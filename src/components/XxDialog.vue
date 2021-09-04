a90b32be5d0218fc18642efe8cf92bf4be454745
DIY熙 <xswork@qq.com>
2021-09-04 19:08:25 +0800

    feat 新增对话框组件

--- src/components/XxDialog.vue
+++ src/components/XxDialog.vue
@@ -4,10 +4,12 @@
         :title="title"
         :visible.sync="dialogVisible"
         :width="width"
-        :before-close="handleClose">
-        <span>这是一段信息</span>
+        :before-close="handleClose"
+        :modal-append-to-body='false'
+        >
+        <xx-input v-for="item in fromData" v-bind:key="item.placeholder"  :placeholder='item.placeholder' v-model="form[item.model]"></xx-input>
         <span slot="footer" class="dialog-footer">
-            <el-button @click="handleClick">取 消</el-button>
+            <el-button @click="handleClose">取 消</el-button>
             <el-button type="primary" @click="handleClick">确 定</el-button>
         </span>
         </el-dialog>
@@ -15,7 +17,9 @@
 </template>
 
 <script>
+import XxInput from './XxInput.vue'
 export default {
+    components: { XxInput },
     props: {
         title: {
             type: String,
@@ -28,13 +32,43 @@ export default {
         dialogVisible: {
             type: Boolean,
             default: false
+        },
+        fromData: {
+            type: Array,
+            default: () => ([])
+        }
+    },
+    watch: {
+        /**
+         * 初始化默认数据
+         * @Ahthor: xiaoxi
+         */
+        fromData() {
+            for (const item of this.fromData) {
+                this.form[item.model] = item.value
+            }
+        }
+    },
+    data() {
+        return {
+            form: {}
         }
     },
     methods: {
+        /**
+         * 点击事件
+         * @Ahthor: xiaoxi
+         */
         handleClick() {
-            this.$emit('click')
+            this.$emit('click', this.form)
         },
-        handleClose() {}
+        /**
+         * 取消事件
+         * @Ahthor: xiaoxi
+         */
+        handleClose() {
+            this.$emit('dialogCancel')
+        }
     }
 }
 </script>
