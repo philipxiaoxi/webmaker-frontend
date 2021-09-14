58527130efa1689ba9ab8d0013da285136fab754
DIY熙 <xswork@qq.com>
2021-09-14 21:12:42 +0800

    feat 添加docker管理api

--- src/api/index.js
+++ src/api/index.js
@@ -2,12 +2,14 @@ const snippet = require('./snippet')
 const user = require('./user')
 const snippetProject = require('./snippetProject')
 const forum = require('./forum')
+const docker = require('./docker')
 const API = {
     server: 'http://www.xiaotao2333.top:8000/',
     snippet: snippet,
     user: user,
     snippetProject: snippetProject,
     forum: forum,
+    docker: docker,
     getServer() {
         return this.server || location.origin
     }
