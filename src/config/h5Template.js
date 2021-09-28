5807e4d4250fb65e17b7550f59d2551b1bc65e83
DIY熙 <xswork@qq.com>
2021-09-29 00:10:51 +0800

    feat 新增片段代码

---
+++ src/config/h5Template.js
@@ -0,0 +1,33 @@
+const html5 =
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+`
+
+const html4 = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+`
+
+export {
+    html4, html5
+}
