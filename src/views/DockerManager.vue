599ba87d8eff07c9c7b20c119b61c03187fc61ab
DIY熙 <xswork@qq.com>
2021-09-29 14:39:48 +0800

    feat 更新docker改为一人一容器一目录

--- src/views/DockerManager.vue
+++ src/views/DockerManager.vue
@@ -1,17 +1,13 @@
 <template>
-    <div class="container">
+    <div class="docker_container">
         <h1>CodeShare Docker Manager</h1>
         <p>由codeshare管控的虚拟容器可以让你在web上开发任何语言。享受你的开发乐趣。</p>
-        <p>
-            测试阶段，只允许一容器运行。更多请查看
-            <el-link type="primary" @click="goTo">使用说明
-            </el-link>
-        </p>
+        <p>更新：当前申请容器是一人一容器一目录，目录代码将会保存。</p>
         <p>容器状态：{{dockerStatus}}，当容器释放后可重新申请。</p>
         <div class="xx-from-container">
             <xx-input v-model="form.password" placeholder="设置连接密码"></xx-input>
             <div>
-                <el-button type="success" round @click="openDocker">打开容器</el-button>
+                <!-- <el-button type="success" round @click="openDocker">打开容器</el-button> -->
                 <el-button type="primary" round @click="createDocker">申请容器</el-button>
             </div>
 
@@ -46,10 +42,10 @@ export default {
             })
             if (res.data.status == 200) {
                 this.$message({
-                    message: '您的容器申请成功，运行在端口' + res.data.data,
+                    message: '您的容器申请成功，运行在端口' + res.data.data.port,
                     type: 'success'
                 })
-                window.open('http://www.xiaotao2333.top:' + res.data.data)
+                window.open('http://www.xiaotao2333.top:' + res.data.data.port)
             }
         },
         openDocker() {
@@ -71,7 +67,7 @@ export default {
 </script>
 
 <style lang='less' scoped>
-.container {
+.docker_container {
     max-width: 1144px;
     padding: 20px;
     margin: 0 auto;
