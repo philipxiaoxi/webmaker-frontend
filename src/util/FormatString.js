9d91788a6f3957f82816a3f9443cccfd21a6524f
DIY熙 <xswork@qq.com>
2021-10-05 12:14:01 +0800

    chore 更名webmaker

--- src/util/FormatString.js
+++ src/util/FormatString.js
@@ -2,9 +2,9 @@ const FS = {
     getIdentityString(identity) {
         switch (identity) {
         case 0:
-            return 'CodeShare管理员'
+            return 'WebMaker管理员'
         case 1:
-            return 'CodeShare金牌认证作者'
+            return 'WebMaker金牌认证作者'
         case 2:
             return '初级实习作者'
         case 3:
