4f16fb3e0298a83d5e9ffea69edbe8f41ebfd5a2
DIY熙 <xswork@qq.com>
2021-11-01 22:00:04 +0800

    feat 新增js代码混淆去除console.log

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,20 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 // vue.config.js
 module.exports = {
     configureWebpack: {
         plugins: [
-            new MonacoWebpackPlugin()
+            new MonacoWebpackPlugin(),
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    warnings: false,
+                    compress: {
+                        drop_console: true,
+                        drop_debugger: false,
+                        pure_funcs: ['console.log']
+                    }
+                }
+            })
         ]
     },
     devServer: {
