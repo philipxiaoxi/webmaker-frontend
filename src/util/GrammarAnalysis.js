ac271d005e252e057078e1a8755ce549fd0e6aaa
DIY熙 <xswork@qq.com>
2021-11-02 10:26:02 +0800

    feat 新增语法树分析

---
+++ src/util/GrammarAnalysis.js
@@ -0,0 +1,32 @@
+/**
+ * 语法分析
+ */
+const esprima = require('esprima')
+const GA = {
+    /**
+     * while for 循环加入熔断函数
+     * @Ahthor: xiaoxi
+     * @param {*} code
+     */
+    addLoopFusing(code) {
+        let whiles = []
+        esprima.parseScript(code, {}, function(node, meta) {
+            if (node.type == 'WhileStatement' || node.type == 'ForStatement') {
+                whiles.push({
+                    start: meta.start.offset,
+                    end: meta.end.offset
+                })
+            }
+        })
+        whiles = whiles.sort((a, b) => { return b.end - a.end })
+        for (const w of whiles) {
+            console.log(w)
+            let pre = code.slice(0, w.end)
+            pre = pre.substring(0, pre.length - 1)
+            pre = pre + 'cs.fus() }'
+            code = pre + code.slice(w.end + 1)
+        }
+        return code
+    }
+}
+export default GA
