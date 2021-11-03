a287af908de04792e6a56a014a9a59dd3cf90f5f
DIY熙 <xswork@qq.com>
2021-11-03 18:02:17 +0800

    feat 新增html片段预设

--- src/config/h5Template.js
+++ src/config/h5Template.js
@@ -13,21 +13,167 @@ const html5 =
 </html>
 `
 
-const html4 = `
-<!DOCTYPE html>
+const vue2Templete =
+`<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Vue 2 Demo</title>
+    <script src="https://cdn.bootcdn.net/ajax/libs/vue/2.6.14/vue.min.js"></script>
 </head>
+
+<!-- 这里写CSS -->
+<style>
+
+</style>
+
 <body>
+    <div id="app">
+        {{ message }}
+    </div>
     
+    <!-- 这里写脚本 -->
+    <script>
+    let app = new Vue({
+        el: '#app',
+        data: {
+            message: 'Hello Vue!'
+        }
+    })
+    </script>
 </body>
 </html>
 `
 
+const vue3Templete =
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vue 3 Demo</title>
+    <script src="https://cdn.bootcdn.net/ajax/libs/vue/3.2.0-beta.7/vue.global.min.js"></script>
+</head>
+
+<!-- 这里写CSS -->
+<style>
+.demo {
+    font-family: sans-serif;
+    border: 1px solid #eee;
+    border-radius: 2px;
+    padding: 20px 30px;
+    margin-top: 1em;
+    margin-bottom: 40px;
+    user-select: none;
+    overflow-x: auto;
+}
+</style>
+
+<body>
+    <div id="app" class="demo">
+        {{ message }}
+    </div>
+    
+    <!-- 这里写脚本 -->
+    <script>
+    const HelloVueApp = {
+        data() {
+            return {
+            message: 'Hello Vue!!'
+            }
+        }
+    }
+    Vue.createApp(HelloVueApp).mount('#app')
+    </script>
+</body>
+</html>
+`
+
+const elementPlus =
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vue 3 elementPlus Demo</title>
+    <!-- 导入样式 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/element-plus/dist/index.css" />
+    <!-- 导入 Vue 3 -->
+    <script src="https://cdn.jsdelivr.net/npm/vue@next"></script>
+    <!-- 导入组件库 -->
+    <script src="https://cdn.jsdelivr.net/npm/element-plus"></script>
+</head>
+
+<!-- 这里写CSS -->
+<style>
+
+</style>
+
+<body>
+    <div id="app">
+        <el-button type="primary">{{ message }}</el-button>
+    </div>
+    
+    <!-- 这里写脚本 -->
+    <script>
+    const HelloVueApp = {
+        data() {
+            return {
+            message: '欢迎使用element-plus'
+            }
+        }
+    }
+    const app = Vue.createApp(HelloVueApp)
+    app.use(ElementPlus)
+    app.mount("#app")
+    </script>
+</body>
+</html>
+`
+
+const elementUi =
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vue 2 element-ui Demo</title>
+    <script src="https://cdn.bootcdn.net/ajax/libs/vue/2.6.14/vue.min.js"></script>
+    <!-- 引入样式 -->
+    <link href="https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.3/theme-chalk/index.min.css" rel="stylesheet">
+    <!-- 引入组件库 -->
+    <script src="https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.3/index.min.js"></script>
+</head>
+
+<!-- 这里写CSS -->
+<style>
+
+</style>
+
+<body>
+    <div id="app">
+        <el-button type="primary">{{ message }}</el-button>
+    </div>
+    
+    <!-- 这里写脚本 -->
+    <script>
+    let app = new Vue({
+        el: '#app',
+        data: {
+            message: '欢迎使用 element-ui!'
+        }
+    })
+    </script>
+</body>
+</html>
+`
+
+
 export {
-    html4, html5
+    vue2Templete, vue3Templete, elementPlus, elementUi, html5
 }
--- src/config/htmlProposals.js
+++ src/config/htmlProposals.js
@@ -1,4 +1,4 @@
-import { html5 } from './h5Template'
+import { vue2Templete, vue3Templete, elementPlus, elementUi, html5 } from './h5Template'
 
 export default {
     getProposals(monaco) {
@@ -8,6 +8,30 @@ export default {
                 kind: monaco.languages.CompletionItemKind.Snippet,
                 documentation: 'html5模板',
                 insertText: html5
+            },
+            {
+                label: 'vue2-templete',
+                kind: monaco.languages.CompletionItemKind.Snippet,
+                documentation: 'vue2模板',
+                insertText: vue2Templete
+            },
+            {
+                label: 'vue3-templete',
+                kind: monaco.languages.CompletionItemKind.Snippet,
+                documentation: 'vue3模板',
+                insertText: vue3Templete
+            },
+            {
+                label: 'vue3-elementPlus',
+                kind: monaco.languages.CompletionItemKind.Snippet,
+                documentation: 'vue3-elementPlus模板',
+                insertText: elementPlus
+            },
+            {
+                label: 'vue2-elementUi',
+                kind: monaco.languages.CompletionItemKind.Snippet,
+                documentation: 'vue3-elementUi模板',
+                insertText: elementUi
             }
         ]
     }
