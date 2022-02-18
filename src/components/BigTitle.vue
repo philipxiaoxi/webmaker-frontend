100ad61a87bacad4134b5626e17aff49f799eec1
DIY熙 <xswork@qq.com>
2022-02-18 11:49:27 +0800

    feat 新增横向大标题

---
+++ src/components/BigTitle.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="title-container flex-row ai-center">
+        <div class="line"></div>
+        <div class="title px-20">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BigTitle',
+    components: {},
+    props: {
+    },
+    data() {
+        return {
+            content: '',
+            editorOption: {}
+        }
+    },
+    computed: {
+    },
+    watch: {
+    },
+    methods: {
+    }
+}
+</script>
+
+<style lang='less' scoped>
+.title-container {
+    text-align: left;
+    &>.line {
+        border-radius: 5px;
+        height: 8px;
+        width: 30px;
+        background-color: darkblue;
+    }
+    &>.title {
+        font-size: 18px;
+        color: #191d21;
+        font-weight: bold;
+    }
+}
+</style>
