46548455b64f95645970d3067669e0fe6a3186f5
DIY熙 <xswork@qq.com>
2022-06-08 14:36:43 +0800

    feat 新增API 配置信息配置模板

--- src/api/index.templete.js
+++ src/api/index.templete.js
@@ -4,10 +4,8 @@ const snippetProject = require('./snippetProject')
 const forum = require('./forum')
 const docker = require('./docker')
 const API = {
-    // server: 'http://www.xiaotao2333.top:8000/',
-    // server: 'https://codeshare.xiaotao2333.top:344/',
-    server: 'https://www.xiaotao2333.top:5885/',
-    wsServer: 'wss://www.xiaotao2333.top:5885/ws',
+    server: 'xxxxxxxxxxx',
+    wsServer: 'xxxxxxxxxxxxxxxxxx',
     snippet: snippet,
     user: user,
     snippetProject: snippetProject,
