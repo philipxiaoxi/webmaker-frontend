026c95e4dfa1b573fe62ed05f7cdfc6a207df798
DIY熙 <xswork@qq.com>
2022-06-14 12:42:01 +0800

    feat 新增多路由匹配

--- src/layout/Header.vue
+++ src/layout/Header.vue
@@ -103,7 +103,7 @@ export default {
                 },
                 {
                     name: '创意',
-                    path: ['/toolbox']
+                    path: ['/toolbox', '/lowcode', '/disk']
                 }
             ],
             routePath: this.$route.path,
